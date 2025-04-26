7c527b3463a44322edeefd555206944b93177b68
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-26 21:57:34 +0400

    initial - preparing for Consulo

--- plugin/src/main/gen/ru/adelf/idea/dotenv/psi/impl/DotEnvNestedVariableKeyImpl.java
+++ plugin/src/main/gen/ru/adelf/idea/dotenv/psi/impl/DotEnvNestedVariableKeyImpl.java
@@ -1,7 +1,6 @@
 // This is a generated file. Not intended for manual editing.
 package ru.adelf.idea.dotenv.psi.impl;
 
-import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
@@ -11,42 +10,20 @@ import static ru.adelf.idea.dotenv.psi.DotEnvTypes.*;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import ru.adelf.idea.dotenv.psi.*;
 
-public class DotEnvPropertyImpl extends ASTWrapperPsiElement implements DotEnvProperty {
+public class DotEnvNestedVariableKeyImpl extends ASTWrapperPsiElement implements DotEnvNestedVariableKey {
 
-  public DotEnvPropertyImpl(@NotNull ASTNode node) {
+  public DotEnvNestedVariableKeyImpl(@NotNull ASTNode node) {
     super(node);
   }
 
   public void accept(@NotNull DotEnvVisitor visitor) {
-    visitor.visitProperty(this);
+    visitor.visitNestedVariableKey(this);
   }
 
   @Override
   public void accept(@NotNull PsiElementVisitor visitor) {
     if (visitor instanceof DotEnvVisitor) accept((DotEnvVisitor)visitor);
     else super.accept(visitor);
-  }
-
-  @Override
-  @NotNull
-  public DotEnvKey getKey() {
-    return findNotNullChildByClass(DotEnvKey.class);
-  }
-
-  @Override
-  @Nullable
-  public DotEnvValue getValue() {
-    return findChildByClass(DotEnvValue.class);
-  }
-
-  @Override
-  public String getKeyText() {
-    return DotEnvPsiUtil.getKeyText(this);
-  }
-
-  @Override
-  public String getValueText() {
-    return DotEnvPsiUtil.getValueText(this);
   }
 
 }
--- plugin/src/main/gen/ru/adelf/idea/dotenv/psi/impl/DotEnvPropertyImpl.java
+++ plugin/src/main/gen/ru/adelf/idea/dotenv/psi/impl/DotEnvPropertyImpl.java
@@ -1,7 +1,6 @@
 // This is a generated file. Not intended for manual editing.
 package ru.adelf.idea.dotenv.psi.impl;
 
-import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
@@ -11,20 +10,42 @@ import static ru.adelf.idea.dotenv.psi.DotEnvTypes.*;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import ru.adelf.idea.dotenv.psi.*;
 
-public class DotEnvNestedVariableKeyImpl extends ASTWrapperPsiElement implements DotEnvNestedVariableKey {
+public class DotEnvPropertyImpl extends ASTWrapperPsiElement implements DotEnvProperty {
 
-  public DotEnvNestedVariableKeyImpl(@NotNull ASTNode node) {
+  public DotEnvPropertyImpl(@NotNull ASTNode node) {
     super(node);
   }
 
   public void accept(@NotNull DotEnvVisitor visitor) {
-    visitor.visitNestedVariableKey(this);
+    visitor.visitProperty(this);
   }
 
   @Override
   public void accept(@NotNull PsiElementVisitor visitor) {
     if (visitor instanceof DotEnvVisitor) accept((DotEnvVisitor)visitor);
     else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public DotEnvKey getKey() {
+    return findNotNullChildByClass(DotEnvKey.class);
+  }
+
+  @Override
+  @Nullable
+  public DotEnvValue getValue() {
+    return findChildByClass(DotEnvValue.class);
+  }
+
+  @Override
+  public String getKeyText() {
+    return DotEnvPsiUtil.getKeyText(this);
+  }
+
+  @Override
+  public String getValueText() {
+    return DotEnvPsiUtil.getValueText(this);
   }
 
 }
